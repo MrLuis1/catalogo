04e541c1ddcfcd17f766c6e118825583b8988dc0
MrLuis <programmerlgp@gmail.com>
2023-06-10 18:18:45 -0400

    descomentado de la proteccion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { AuthGuard } from './core/guards/auth.guard';
 const routes: Routes = [
   { path: 'public', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
   { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
-  { path: 'private', loadChildren: () => import('./private/private.module').then(m => m.PrivateModule),  }, 
-  // canActivate: [AuthGuard], canLoad: [AuthGuard]
+  { path: 'private', loadChildren: () => import('./private/private.module').then(m => m.PrivateModule), canActivate: [AuthGuard], canLoad: [AuthGuard] }, 
   { path: '**', redirectTo: 'public/home', pathMatch: 'full' },
 
 ];
